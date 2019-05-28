3559106b8eef774b372bccf961c9e4fc0cd916cb
Marthen van der Eems <marvan-d@c3r6s7.wethinkcode.co.za>
2019-05-28 17:06:16 +0200

    fixed ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: marvan-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:02:38 by marvan-d          #+#    #+#             */
-/*   Updated: 2019/05/21 12:02:40 by marvan-d         ###   ########.fr       */
+/*   Updated: 2019/05/28 16:52:49 by marvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,21 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t i;
+	size_t j;
 
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	while (*haystack != '\0')
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)&haystack[i]);
+	while (haystack[i] != '\0' && i < len)
 	{
-		i = 0;
-		while (haystack[i] == needle[i] && i < len)
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
 		{
-			if (needle[i + 1] == '\0')
-				return ((char *)haystack);
-			i++;
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
 		}
-		haystack++;
+		i++;
 	}
 	return (NULL);
 }
