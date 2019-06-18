99c9184fa5328d1fbb2236f51a3993f63fe7e7ef
Marthen van der Eems <marvan-d@c3r7s6.wethinkcode.co.za>
2019-06-18 15:53:54 +0200

    updated lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: marvan-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 12:06:19 by marvan-d          #+#    #+#             */
-/*   Updated: 2019/06/05 12:06:24 by marvan-d         ###   ########.fr       */
+/*   Updated: 2019/06/18 15:31:51 by marvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 	if (*alst)
 	{
 		del((*alst)->content, (*alst)->content_size);
-		free(*alst);
 		*alst = NULL;
 	}
 }
